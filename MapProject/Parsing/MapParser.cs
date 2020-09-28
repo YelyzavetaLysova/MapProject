830e67ce8ef97293c562739db24c7b0c7e158fd1
MLY <matveylysov@gmail.com>
2020-09-29 01:57:43 +0800

    Breakthrough in out project

--- MapProject/Parsing/MapParser.cs
+++ MapProject/Parsing/MapParser.cs
@@ -31,7 +31,9 @@ namespace MapProject.Parsing
 
                 for (int x = 0; x < img.Width; x++)
                 {
-                    this._points[x, y] = new MapProject.Model.Point(x, y, this.isBorder(new Color(pixelsRow[x])));
+                    
+                    this._points[x, y] = new MapProject.Model.Point(x, y, (x == 0 || y == 0 || x == img.Width - 1 || y == img.Height - 1) ? true : this.isBorder(new Color(pixelsRow[x])));
+                    
                 }
             }
 
@@ -86,21 +88,89 @@ namespace MapProject.Parsing
 
             ImageHelper.SaveRegionBordersToImage(regions, 100);
 
+            Console.WriteLine("Borders pre-processing started...");
+
+            int ii = 0;
+
+            for(int i = 0; i<regions.Count(); i++)
+            {
+                var region = regions[i];
+
+                Console.WriteLine("Pre-processing region " + ++ii + "/" + regions.Count());
+                var pointsToRemove = new List<Model.Point>();
+
+                var points = region.Points.Where(x => x.IsBorder).ToArray();
+
+                for(int j = 0; j < points.Length; j++)
+                {
+                    var point = points[j];
+
+                    var aroundBorderPoints = this.GetAroundBorderPoints(point, region);
+                    var aroundInternalPoints = this.GetAroundPoints(point, region);
+                    var allAroundPoints = this.GetAroundPoints(point);
+                    
+                    if (aroundInternalPoints.Count(x => x.IsBorder == false) == 0)
+                    {
+                        pointsToRemove.Add(point);
+                        point.ParentId = null;
+
+                        continue;
+                    }
+
+                    if (aroundBorderPoints.Count() == 1)
+                    {
+                        pointsToRemove.Add(point);
+                        point.ParentId = null;
+                        continue;
+                    }
+
+                    //if (allAroundPoints.Where(x => x.IsBorder == false).All(x => region.Points.Contains(x)))
+                    //{
+                    //    pointsToRemove.Add(point);
+                    //    continue;
+                    //}
+                }
+
+                
+
+                foreach (var pointToRemove in pointsToRemove)
+                {
+                    region.Points.Remove(pointToRemove);
+
+                    //if (region.Points.Any(x => x.X == pointToRemove.X && x.Y == pointToRemove.Y))
+                    //{
+                    //    region.Points.RemoveAll(x => x.X == pointToRemove.X && x.Y == pointToRemove.Y);
+                    //}
+
+                   
+                }
+            }
+
+            Console.WriteLine("Borders pre-processing finished");
+
             foreach (var region in regions)
             {
                 List<Contur> sealedConturs = this.FindSealedConturs(region).ToList();
                 sealedConturs.RemoveAll(x => x.PointsList.Count() < 3);
-                int i = 0;
-                Console.WriteLine("I am working...");
+                //int i = 0;
+                Console.WriteLine("I am workingre...");
 
                 foreach (var contur in sealedConturs)
                 {
                     ImageHelper.SavePointsToImage(contur.PointsList);
                 }
 
+                region.Points.Clear();
+
+                region.Points.AddRange(sealedConturs.OrderByDescending(x => x.PointsList.Count).First().PointsList);
+
             }
 
-            
+            Console.WriteLine("count2: " + this.count2);
+
+            Console.WriteLine("Done.");
+
+
 
 
 
@@ -184,48 +254,61 @@ namespace MapProject.Parsing
             return true;
         }
         
-        private IEnumerable<MapProject.Model.Point> GetAroundPoints(MapProject.Model.Point p)
+        private IEnumerable<MapProject.Model.Point> GetAroundPoints(MapProject.Model.Point point, Region region = null, bool directOnly = false)
         {
             List<MapProject.Model.Point> result = new List<MapProject.Model.Point>();
 
-            if (p.X != 0)
-            {
-                result.Add(this._points[p.X - 1, p.Y]);
-            }
+            //if (region == null)
+            //{
 
-            if (p.X != this._points.GetLength(0) - 1)
-            {
-                result.Add(this._points[p.X + 1, p.Y]);
-            }
+                if (point.X != 0)
+                {
+                    result.Add(this._points[point.X - 1, point.Y]);
+                }
 
-            if (p.Y != 0)
-            {
-                result.Add(this._points[p.X, p.Y - 1]);
-            }
+                if (point.X != this._points.GetLength(0) - 1)
+                {
+                    result.Add(this._points[point.X + 1, point.Y]);
+                }
 
-            if (p.Y != this._points.GetLength(1) - 1)
-            {
-                result.Add(this._points[p.X, p.Y + 1]);
-            }
+                if (point.Y != 0)
+                {
+                    result.Add(this._points[point.X, point.Y - 1]);
+                }
 
-            if (p.X != 0 && p.Y != 0)
-            {
-                result.Add(this._points[p.X - 1, p.Y - 1]);
-            }
+                if (point.Y != this._points.GetLength(1) - 1)
+                {
+                    result.Add(this._points[point.X, point.Y + 1]);
+                }
 
-            if (p.X != this._points.GetLength(0) - 1 && p.Y != 0)
+            if (!directOnly)
             {
-                result.Add(this._points[p.X + 1, p.Y - 1]);
-            }
 
-            if (p.Y != this._points.GetLength(1) - 1 && p.X != 0)
-            {
-                result.Add(this._points[p.X - 1, p.Y + 1]);
-            }
+                if (point.X != 0 && point.Y != 0)
+                {
+                    result.Add(this._points[point.X - 1, point.Y - 1]);
+                }
 
-            if (p.Y != this._points.GetLength(1) - 1 && p.X != this._points.GetLength(0) - 1)
+                if (point.X != this._points.GetLength(0) - 1 && point.Y != 0)
+                {
+                    result.Add(this._points[point.X + 1, point.Y - 1]);
+                }
+
+                if (point.Y != this._points.GetLength(1) - 1 && point.X != 0)
+                {
+                    result.Add(this._points[point.X - 1, point.Y + 1]);
+                }
+
+                if (point.Y != this._points.GetLength(1) - 1 && point.X != this._points.GetLength(0) - 1)
+                {
+                    result.Add(this._points[point.X + 1, point.Y + 1]);
+                }
+
+            }
+        
+            if (region != null)
             {
-                result.Add(this._points[p.X + 1, p.Y + 1]);
+                return result.Where(x => x.ParentId == region.Id).ToList();
             }
 
             return result;
@@ -234,59 +317,132 @@ namespace MapProject.Parsing
 
 
 
-        class Contur
+        private class Contur
         {
-            public List<Model.Point> PointsList
+            public HashSet<Model.Point> PointsList
             {
-                private set;
                 get;
+                private set;
             }
 
             public Contur()
             {
-                this.PointsList = new List<Model.Point>();
+                this.PointsList = new HashSet<Model.Point>();
+            }
+
+            public Contur CopyContur()
+            {
+                Contur result = new Contur();
+
+                foreach(var element in this.PointsList)
+                {
+                    result.PointsList.Add(element);
+                }
+
+                return result;
             }
         }
 
-        IEnumerable<Model.Point> GetAroundBorderPoints(Model.Point point, Model.Region region)
+        IEnumerable<Model.Point> GetAroundBorderPoints(Model.Point point, Model.Region region, bool directOnly = false)
         {
-            //List<Model.Point> result = new List<Model.Point>();
-
-            var result = region.Points.Where(x => x.IsBorder).Where(x =>
-            {
-                if ((point.X == x.X && point.Y == x.Y + 1) || (point.X == x.X + 1 && point.Y == x.Y) || (point.X == x.X - 1 && point.Y == x.Y) || (point.X == x.X && point.Y == x.Y - 1)
-                || (point.X == x.X + 1 && point.Y == x.Y + 1) || (point.X == x.X + 1 && point.Y == x.Y - 1) || (point.X == x.X - 1 && point.Y == x.Y - 1) || (point.X == x.X - 1 && point.Y == x.Y + 1))
-                {
-                    return true;
-                }
-                return false;
-            });
-            return result;
+            return this.GetAroundPoints(point, region, directOnly).Where(x => x.IsBorder);
         }
+
+        //IEnumerable<Model.Point> GetAroundBorderPoints(Model.Point point, Model.Region region)
+        //{
+        //    List<MapProject.Model.Point> result = new List<MapProject.Model.Point>();
+
+        //    if (point.X != 0)
+        //    {
+        //        result.Add(this._points[point.X - 1, point.Y]);
+        //    }
+
+        //    if (point.X != this._points.GetLength(0) - 1)
+        //    {
+        //        result.Add(this._points[point.X + 1, point.Y]);
+        //    }
+
+        //    if (point.Y != 0)
+        //    {
+        //        result.Add(this._points[point.X, point.Y - 1]);
+        //    }
+
+        //    if (point.Y != this._points.GetLength(1) - 1)
+        //    {
+        //        result.Add(this._points[point.X, point.Y + 1]);
+        //    }
+
+        //    if (point.X != 0 && point.Y != 0)
+        //    {
+        //        result.Add(this._points[point.X - 1, point.Y - 1]);
+        //    }
+
+        //    if (point.X != this._points.GetLength(0) - 1 && point.Y != 0)
+        //    {
+        //        result.Add(this._points[point.X + 1, point.Y - 1]);
+        //    }
+
+        //    if (point.Y != this._points.GetLength(1) - 1 && point.X != 0)
+        //    {
+        //        result.Add(this._points[point.X - 1, point.Y + 1]);
+        //    }
+
+        //    if (point.Y != this._points.GetLength(1) - 1 && point.X != this._points.GetLength(0) - 1)
+        //    {
+        //        result.Add(this._points[point.X + 1, point.Y + 1]);
+        //    }
+
+        //    if (region != null)
+        //    {
+        //        return result.Where(x => x.IsBorder == true && x.ParentId == region.Id).ToList();
+        //    }
+
+        //    return result;
+        //}
 
         IEnumerable<Contur> FindSealedConturs(Model.Region region)
         {
             List<Contur> result = new List<Contur>();
 
-
-
             foreach (var point in region.Points.Where(x => x.IsBorder))
             {
+
+                List<Contur> conturs = new List<Contur>();
+
                 Contur contur = new Contur();
                 //contur.PointsList.Add(point);
-                this.ProcessBorderPoints(point, contur, region);
+
+                if (this.GetAroundBorderPoints(point, region, true).Count() == 2)
+                {
+                    //if (!conturs.Any(x => x.PointsList.Contains(point)))
+                    //{
+                        this.ProcessBorderPoints(point, contur, region, /*conturs,*/ true);
+                    //}
+                }
+
+                //if (conturs.Count != 0)
+                //{
+                //    result.Add(conturs.OrderByDescending(x => x.PointsList.Count()).First());
+                //}
 
                 if (contur.PointsList.Count() != 0)
                 {
                     result.Add(contur);
+
                 }
             }
+
+            
 
             return result;
         }
 
-        private void ProcessBorderPoints(Model.Point point, Contur contur, Model.Region region)
+
+        private int count2 = 0;
+
+        private void ProcessBorderPoints(Model.Point point, Contur contur, Model.Region region, bool first = false)
         {
+
 
             if (!this._processedBorderPoints.Contains(point))
             {
@@ -296,18 +452,168 @@ namespace MapProject.Parsing
                 _processedBorderPoints.Add(point);
 
 
-                var points = this.GetAroundBorderPoints(point, region);
+                var nextPoints = this.GetAroundBorderPoints(point, region, true);
 
-                if (points.All(x => this._processedBorderPoints.Contains(x)))
+                if (first)
+                {
+                    nextPoints = new List<Model.Point> { nextPoints.First() };
+                }
+
+                if (nextPoints.All(x => this._processedBorderPoints.Contains(x)))
                 {
                     return;
                 }
 
-                foreach (var nextPoint in points.Where(x => !this._processedBorderPoints.Contains(x)))
+                if (nextPoints.Count() > 2)
+                {
+                    count2++;
+                }
+
+                foreach (var nextPoint in nextPoints.Where(x => !this._processedBorderPoints.Contains(x)))
                 {
                     this.ProcessBorderPoints(nextPoint, contur, region);
                 }
             }
+
+
         }
+
+        //private void ProcessBorderPoints(Model.Point point, Contur contur, Model.Region region, List<Contur> conturs, bool first = false)
+        //{
+
+        //    if (!conturs.Contains(contur))
+        //    {
+        //        conturs.Add(contur);
+        //    }
+
+        //    if (!contur.PointsList.Contains(point))
+        //    {
+        //        contur.PointsList.Add(point);
+
+        //        IEnumerable<Model.Point> nextPoints = this.GetAroundBorderPoints(point, region).Where(x => !contur.PointsList.Contains(x)).ToList();
+
+        //        if (nextPoints.Count() == 0)
+        //        {
+        //            return;
+        //        }
+
+        //        if (first)
+        //        {
+        //            nextPoints = new List<Model.Point> { nextPoints.First() };
+        //        }
+
+        //        if (nextPoints.Any())
+        //        {
+        //            if (nextPoints.Count() > 1)
+        //            {
+        //                foreach (var nextPoint in nextPoints)
+        //                {
+        //                    Contur newContur = contur.CopyContur();
+
+        //                    this.ProcessBorderPoints(nextPoint, newContur, region, conturs);
+        //                }
+        //            }
+        //            else
+        //            {
+        //                this.ProcessBorderPoints(nextPoints.First(), contur, region, conturs);
+        //            }
+
+
+        //        }
+        //    }
+        //}
+
+        //private bool CheckInternalIntersection(Model.Point currentPoint, Model.Point nextPoint, Region region)
+        //{
+        //    var currentInternalPoints = new HashSet<Model.Point>(this.GetAroundPoints(currentPoint, region).Where(x => !x.IsBorder));
+
+        //    var nextInternalPoints = new HashSet<Model.Point>((this.GetAroundPoints(nextPoint, region).Where(x => !x.IsBorder)));
+
+        //    return currentInternalPoints.Overlaps(nextInternalPoints);
+        //}
+
+        //private bool CheckExternalIntersection(Model.Point currentPoint, Model.Point nextPoint, Region region)
+        //{
+        //    var currentExternalPoints = new HashSet<Model.Point>(this.GetAroundPoints(currentPoint).Where(x => !region.Points.Contains(x)));
+
+        //    var nextExternalPoints = new HashSet<Model.Point>(this.GetAroundPoints(nextPoint).Where(x => !region.Points.Contains(x)));
+
+        //    return currentExternalPoints.Overlaps(nextExternalPoints);
+        //}
+
+        //private void ProcessBorderPoints(Model.Point point, Contur contur, Model.Region region)
+        //{
+
+        //    if (!this._processedBorderPoints.Contains(point))
+        //    {
+
+        //        contur.PointsList.Add(point);
+
+        //        _processedBorderPoints.Add(point);
+
+        //        //1. Выбрать все окружающие точки с общими внутренними и внешними точками
+
+
+
+        //        //var nextInter
+
+
+        //        var points = this.GetAroundBorderPoints(point, region);
+
+        //        if (points.All(x => this._processedBorderPoints.Contains(x)))
+        //        {
+        //            return;
+        //        }
+
+
+
+        //        //var test = points.Where(x => this.CheckInternalIntersection(point, x, region));
+
+        //        bool pointProcessed = false;
+
+        //        foreach (var nextPoint in points)
+        //        {
+        //            var externalPoints = this.GetAroundPoints(nextPoint);
+
+        //            if (!pointProcessed && externalPoints.Any(x => ! region.Points.Contains(x)))
+        //            {
+        //                pointProcessed = true;
+
+        //                this.ProcessBorderPoints(nextPoint, contur, region);
+        //            }
+        //            else
+        //            {
+        //                this._processedBorderPoints.Add(nextPoint);
+        //            }
+
+        //        }
+        //    }
+        //}
+
+
+        //private void ProcessBorderPoints(Model.Point point, Contur contur, Model.Region region)
+        //{
+
+        //    if (!this._processedBorderPoints.Contains(point))
+        //    {
+
+        //        contur.PointsList.Add(point);
+
+        //        _processedBorderPoints.Add(point);
+
+
+        //        var points = this.GetAroundBorderPoints(point, region);
+
+        //        if (points.All(x => this._processedBorderPoints.Contains(x)))
+        //        {
+        //            return;
+        //        }
+
+        //        foreach (var nextPoint in points.Where(x => !this._processedBorderPoints.Contains(x)))
+        //        {
+        //            this.ProcessBorderPoints(nextPoint, contur, region);
+        //        }
+        //    }
+        //}
     }
 }
